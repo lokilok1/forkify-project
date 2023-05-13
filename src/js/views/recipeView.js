dc61924663028f9b660790de3ed7c11d13d7f538
Ricky Wong <rickywonglt15@outlook.com>
2023-05-13 18:39:36 -0400

    æadd numberToFraction

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,5 @@
 import View from './View.js'
-import fracty from 'fracty';
+import { numberToFraction } from '../helpers.js';
 
 // import icons from '../img/icons.svg'; // Parcel 1
 import icons from "../../img/icons.svg";
@@ -119,7 +119,7 @@ class RecipeView extends View {
               <svg class="recipe__icon">
                 <use href="${icons}#icon-check"></use>
               </svg>
-              <div class="recipe__quantity">${ ing.quantity ? fracty(ing.quantity).toString() : ""}</div>
+              <div class="recipe__quantity">${ing.quantity ? numberToFraction(ing.quantity).toString() : '' }</div>
               <div class="recipe__description">
                 <span class="recipe__unit">${ing.unit}</span>
                 ${ing.description}
